8587c7d8c775699d103af0191513c4078c44787d
wizardholy <wizardholy@163.com>
2016-08-26 22:24:32 +0800

    集成分享功能

--- app/src/main/java/com/klisly/bookbox/ui/DetailFragment.java
+++ app/src/main/java/com/klisly/bookbox/ui/DetailFragment.java
@@ -38,6 +38,8 @@ import java.util.Date;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 import timber.log.Timber;
@@ -229,7 +231,7 @@ public class DetailFragment extends BaseBackFragment implements Toolbar.OnMenuIt
                         break;
 
                     case R.id.action_share:
-                        ToastHelper.showShortTip(R.string.share);
+                        shareArticle();
                         break;
 
                     case R.id.action_original:
@@ -247,6 +249,36 @@ public class DetailFragment extends BaseBackFragment implements Toolbar.OnMenuIt
             }
         });
         popupMenu.show();
+    }
+
+    private void shareArticle() {
+        String shareUrl = "luckyraider.com";
+        ShareSDK.initSDK(getActivity());
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+//        oks.disableSSOWhenAuthorize();
+
+        // 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+//        oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle("好文大家读");
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl(shareUrl);
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("\""+mData.getArticle().getTitle()+"\""+",在这里发现的一篇美文."+mData.getArticle().getSrcUrl());
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl(shareUrl);
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我发现了这篇很走心,分享给各位!");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl(shareUrl);
+
+        // 启动分享GUI
+        oks.show(getContext());
     }
 
     private void toggleCollect() {
